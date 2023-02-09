f068fee49d248899b5045acc47b35e34b5f63d20
Adel <bordbariadel@gmail.com>
2023-02-10 00:38:51 +0330

    upload final sent version

--- RUNME_dwt_compression.m
+++ RUNME_dwt_compression.m
@@ -1,58 +1,238 @@
-load woman; %sculpture woman mask
-n = 5;
-w = 'db3';
+clear;
 
-[cr,bpp] = wcompress('c',X,'mask.wtc','lvl_mmc','wname',w,'level',n);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%    COMPRESSION    %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% 'c' compress, 'u' uncompress
-% gbl_mmc_f , gbl_mmc_h: for global
-% wcompress('c', x, cname, compmthd, level, it)
-% it: image type transform
+% read base image
+% dwt 3/5-L on base images
+% quantize coeffs (local/global)
+% count zero value-ed coeffs
+% calculate mse/psnr
+% save and show result
 
-% compmthd:
-%'ezw'  Embedded Zerotree Wavelet
-%'spiht'    Set Partitioning In Hierarchical Trees
-%'stw'	Spatial-orientation Tree Wavelet
-%'wdr'	Wavelet Difference Reduction
-%'aswdr'	Adaptively Scanned Wavelet Difference Reduction
-%'spiht_3d'	Set Partitioning In Hierarchical Trees 3D for truecolor images
-%'lvl_mmc'	Subband thresholding of coefficients and Huffman encoding
-%'gbl_mmc_f'	Global thresholding of coefficients and fixed encoding
-%'gbl_mmc_h'	Global thresholding of coefficients and Huffman encoding
+% set to 1,2,3 for different images
+in = 2;
 
-% wname:
-%Haar                     		haar    
-%Daubechies               		db      
-%more: https://www.mathworks.com/help/wavelet/ref/waveletfamilies.html 
+fam = 'db3'; % wavelet family
+lvl = 5; % wavelet depth
 
-Xc = wcompress('u','mask.wtc');
-delete('mask.wtc')
+% set to 1 to apply global thr
+thr_type = 1;
 
+% global threshold value
+gthr = 100;
 
-thr = adaptthresh(Xc); %local threshold
-% set thr manually for global thr
-Xt = wthresh(Xc,'h',thr); %apply threshold
+% read images
+if in == 1
+    x = 'rail.jpg';
+elseif in == 2
+    x = 'disco.jpg';
+else
+    x = 'circ.jpg';
+end
 
-X = double(X);
-D = abs(X-Xc).^2;
-mse  = sum(D(:))/numel(X);
+% read image
+base = imread(x);
+
+% apply dwt on base image
+dc = wavedec3(base, lvl, fam);
+
+% extract coeffs
+lll_base = abs(dc.dec{1});
+hll_base = abs(dc.dec{2});
+lhl_base = abs(dc.dec{3});
+hhl_base = abs(dc.dec{4});
+llh_base = abs(dc.dec{5});
+hlh_base = abs(dc.dec{6});
+lhh_base = abs(dc.dec{7});
+hhh_base = abs(dc.dec{8});
+
+% find 2D standard deviation for all coeff. matrices
+lll_std = stdfilt(lll_base);
+hll_std = stdfilt(hll_base);
+lhl_std = stdfilt(lhl_base);
+hhl_std = stdfilt(hhl_base);
+llh_std = stdfilt(llh_base);
+hlh_std = stdfilt(hlh_base);
+lhh_std = stdfilt(lhh_base);
+hhh_std = stdfilt(hhh_base);
+
+% count number of elements
+lll_n = numel(lll_base);
+hll_n = numel(hll_base);
+lhl_n = numel(lhl_base);
+hhl_n = numel(hhl_base);
+llh_n = numel(llh_base);
+hlh_n = numel(hlh_base);
+lhh_n = numel(lhh_base);
+hhh_n = numel(hhh_base);
+
+% find local threshold
+lll_t = lll_std * (sqrt(2 * log2(lll_n)));
+hll_t = hll_std * (sqrt(2 * log2(hll_n)));
+lhl_t = lhl_std * (sqrt(2 * log2(lhl_n)));
+hhl_t = hhl_std * (sqrt(2 * log2(hhl_n)));
+llh_t = llh_std * (sqrt(2 * log2(llh_n)));
+hlh_t = hlh_std * (sqrt(2 * log2(hlh_n)));
+lhh_t = lhh_std * (sqrt(2 * log2(lhh_n)));
+hhh_t = hhh_std * (sqrt(2 * log2(hhh_n)));
+
+% if global
+if thr_type == 1
+    lll_t = gthr;
+    hll_t = gthr;
+    lhl_t = gthr;
+    hhl_t = gthr;
+    llh_t = gthr;
+    hlh_t = gthr;
+    lhh_t = gthr;
+    hhh_t = gthr;
+end
+
+% find each matrix's size
+lll_size = size(lll_base);
+hll_size = size(hll_base);
+lhl_size = size(lhl_base);
+hhl_size = size(hhl_base);
+llh_size = size(llh_base);
+hlh_size = size(hlh_base);
+lhh_size = size(lhh_base);
+hhh_size = size(hhh_base);
+
+% count zero values in new band matrices
+lll_zeros = sum(lll_base==0,'all');
+hll_zeros = sum(hll_base==0,'all');
+lhl_zeros = sum(lhl_base==0,'all');
+hhl_zeros = sum(hhl_base==0,'all');
+llh_zeros = sum(llh_base==0,'all');
+hlh_zeros = sum(hlh_base==0,'all');
+lhh_zeros = sum(lhh_base==0,'all');
+hhh_zeros = sum(hhh_base==0,'all');
+
+% local threshold for 2D decomposition can be
+% found WITHOUT the formula, using Birge-Massart strategy
+% using wdcbm2
+% but we're reading colors too so, irrelevent.
+
+% apply thresholding on bands
+% if new value < thr => 0
+% otherwise, keep the previous value
+lll_new = lll_base .* double(lll_base > lll_t);
+hll_new = hll_base .* double(hll_base > lll_t);
+lhl_new = lhl_base .* double(lhl_base > lll_t);
+hhl_new = hhl_base .* double(hhl_base > lll_t);
+llh_new = llh_base .* double(llh_base > lll_t);
+hlh_new = hlh_base .* double(hlh_base > lll_t);
+lhh_new = lhh_base .* double(lhh_base > lll_t);
+hhh_new = hhh_base .* double(hhh_base > lll_t);
+
+% count number of zeros for new matrices
+lll_new_zeros = sum(lll_new==0,'all');
+hll_new_zeros = sum(hll_new==0,'all');
+lhl_new_zeros = sum(lhl_new==0,'all');
+hhl_new_zeros = sum(hhl_new==0,'all');
+llh_new_zeros = sum(llh_new==0,'all');
+hlh_new_zeros = sum(hlh_new==0,'all');
+lhh_new_zeros = sum(lhh_new==0,'all');
+hhh_new_zeros = sum(hhh_new==0,'all');
+
+% count how many coeff. were thresholded
+lll_zeros_diff = abs(lll_zeros - lll_new_zeros);
+hll_zeros_diff = abs(hll_zeros - hll_new_zeros);
+lhl_zeros_diff = abs(lhl_zeros - lhl_new_zeros);
+hhl_zeros_diff = abs(hhl_zeros - hhl_new_zeros);
+llh_zeros_diff = abs(llh_zeros - llh_new_zeros);
+hlh_zeros_diff = abs(hlh_zeros - hlh_new_zeros);
+lhh_zeros_diff = abs(lhh_zeros - lhh_new_zeros);
+hhh_zeros_diff = abs(hhh_zeros - hhh_new_zeros);
+
+% show coeff. matrices vs. thresholded version
+figure
+colormap(gray);
+subplot(2,8,1); image(lll_base(:,:,1)); title('LLL');axis square;
+subplot(2,8,2); image(hll_base(:,:,1)); title('HLL');axis square;
+subplot(2,8,3); image(lhl_base(:,:,1)); title('LHL');axis square;
+subplot(2,8,4); image(hhl_base(:,:,1)); title('HHL');axis square;
+subplot(2,8,5); image(llh_base(:,:,1)); title('LLH');axis square;
+subplot(2,8,6); image(hlh_base(:,:,1)); title('HLH');axis square;
+subplot(2,8,7); image(lhh_base(:,:,1)); title('LHH');axis square;
+subplot(2,8,8); image(hhh_base(:,:,1)); title('HHH');axis square;
+subplot(2,8,9); image(lll_new(:,:,1));
+title({'LLL thr';lll_zeros_diff});axis square;
+subplot(2,8,10); image(hll_new(:,:,1));
+title({'HLL thr';hll_zeros_diff});axis square;
+subplot(2,8,11); image(lhl_new(:,:,1));
+title({'LHL thr';lhl_zeros_diff});axis square;
+subplot(2,8,12); image(hhl_new(:,:,1));
+title({'HHL thr';hhl_zeros_diff});axis square;
+subplot(2,8,13); image(llh_new(:,:,1));
+title({'LLH thr';llh_zeros_diff});axis square;
+subplot(2,8,14); image(hlh_new(:,:,1));
+title({'HLH thr';hlh_zeros_diff});axis square;
+subplot(2,8,15); image(lhh_new(:,:,1));
+title({'LHH thr';lhh_zeros_diff});axis square;
+subplot(2,8,16); image(hhh_new(:,:,1));
+title({'HHH thr';hhh_zeros_diff});axis square;
+
+% set new band matrices
+dc.dec{1} = lll_new;
+dc.dec{2} = hll_new;
+dc.dec{3} = lhl_new;
+dc.dec{4} = hhl_new;
+dc.dec{5} = llh_new;
+dc.dec{6} = hlh_new;
+dc.dec{7} = lhh_new;
+dc.dec{8} = hhh_new;
+
+% idwt to reconstruct compressed image
+cmp = waverec3(dc);
+cmp = uint8(cmp);
+
+% calculate mse/psnr
+D = abs(cmp - base) .^2;
+mse  = sum(D(:))/numel(base);
 psnr = 10*log10(255*255/mse);
 
-figure
-image(X);
-title("Org");
-colormap(map);
+% calculate ratio based on number of zero-ed values
+all_zeros = lll_zeros_diff + hll_zeros_diff + ...
+    lhl_zeros_diff + hhl_zeros_diff + ...
+    llh_zeros_diff + hlh_zeros_diff + ...
+    lhh_zeros_diff + hhh_zeros_diff;
+all_elements = numel(base);
+cr = 100 * (all_zeros / all_elements);
 
+% show images and metrics
 figure
-image(Xc);
-title(strcat(num2str(cr),'|',num2str(mse),'|',num2str(psnr)));
-colormap(map);
+subplot(1,2,1);
+imshow(base); axis square;
+title("Original");
+subplot(1,2,2);
+imshow(cmp); axis square;
+msg = strcat("MSE: ", num2str(mse), " | PSNR: ", num2str(psnr));
+title({strcat("Ratio: %", num2str(cr));msg});
 
-D = abs(X-Xt).^2;
-thr_mse  = sum(D(:))/numel(X);
-thr_psnr = 10*log10(255*255/mse);
+% save image locally
+imwrite(cmp, 'compressed.jpg');
 
-figure
-image(Xt);
-title(strcat("thr", '|', num2str(thr_mse),'|',num2str(thr_psnr)));
-colormap(map);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Easier, copy-pasted script: %%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%load woman;
+%fam='haar';lvl=3;
+%[cr,bpp] = wcompress('c',X,'mask.wtc','lvl_mmc','wname',fam,'level',lvl);
+%Xc = wcompress('u','mask.wtc');
+%delete('mask.wtc');
+%colormap(pink(255))
+%subplot(1,2,1); image(X);  title('Original image')
+%axis square
+%subplot(1,2,2); image(Xc); title('Compressed image')
+%axis square
+%D = abs(X-Xc).^2;
+%mse  = sum(D(:))/numel(X);
+%psnr = 10*log10(255*255/mse);
+%mse
+%psnr
--- RUNME_dwt_watermarking.m
+++ RUNME_dwt_watermarking.m
@@ -1,29 +1,185 @@
-A = 'disco.jpg';
-B = 'circle.png';
-C = 'watermarked.png';
+clear;
 
-wv = 'haar';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%    WATERMARKING   %%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-host=imread(A);
-[m n p]=size(host);
+% read cover-wm
+% dwt 3-L both images
+% res = k*cover + q*wm to merge
+% inset result's LL3
+% save and show result
 
-subplot(1,3,1)
-imshow(host);
-title('Original Image');
+% read images
+cover = imread('circ.jpg');
+wm = imread('disco.jpg');
+fam = 'db3'; % wavelet family
+lvl = 3; % wavelet depth
+k = 1; % 0.2, 0.6, 1.0, 1.4, 1.8
+q = 0.01; % 0.009, 0.01
 
-[host_LL,host_LH,host_HL,host_HH]=dwt2(host,wv);
-water_mark=imread(B);
-water_mark=imresize(water_mark,[m n]);
+% show images
+%figure
+%subplot(1,2,1);
+%imshow(cover);
+%title("Cover");
+%subplot(1,2,2);
+%imshow(wm);
+%title("Watermark");
 
-subplot(1,3,2)
-imshow(water_mark)
-title('Watermark');
-[water_mark_LL,water_mark_LH,water_mark_HL,water_mark_HH]=dwt2(water_mark,wv);
-water_marked_LL = host_LL + (0.03*water_mark_LL);
-watermarked=idwt2(water_marked_LL,host_LH,host_HL,host_HH,wv);
+% convert to grayscale
+%cover = rgb2gray(cover);
+%wm = rgb2gray(wm);
 
-subplot(1,3,3)
-imshow(uint8(watermarked));
-title('Watermarked image');
+% apply 2D wavelet decomposition on images
+[c_cover, s_cover] = wavedec2(cover, lvl, fam);
 
-imwrite(uint8(watermarked),'watermarked.png');
+% extract LL coeffs
+ll_cover = appcoef2(c_cover, s_cover, fam, lvl);
+% extract detail coeffs (LH, HL, HH)
+[lh_cover, hl_cover, hh_cover] = detcoef2('all', c_cover, s_cover, lvl);
+
+% get colormap range
+rng = size(cover, 1); %576 for disco.jpg
+% rescale coeff. matrices to original color range
+ll_cover_scaled = wcodemat(ll_cover, rng, 'mat', lvl);
+lh_cover_scaled = wcodemat(lh_cover, rng, 'mat', lvl);
+hl_cover_scaled = wcodemat(hl_cover, rng, 'mat', lvl);
+hh_cover_scaled = wcodemat(hh_cover, rng, 'mat', lvl);
+
+% do everything the same for watermark image
+[c_wm, s_wm] = wavedec2(wm, lvl, fam);
+ll_wm = appcoef2(c_wm, s_wm, fam, lvl);
+[lh_wm, hl_wm, hh_wm] = detcoef2('all', c_wm, s_wm, lvl);
+wm_rng = size(wm, 1);
+ll_wm_scaled = wcodemat(ll_wm, wm_rng, 'mat', 3);
+lh_wm_scaled = wcodemat(lh_wm, wm_rng, 'mat', 3);
+hl_wm_scaled = wcodemat(hl_wm, wm_rng, 'mat', 3);
+hh_wm_scaled = wcodemat(hh_wm, wm_rng, 'mat', 3);
+
+%figure
+%colormap pink(255);
+%subplot(2,4,1); imagesc(ll_wm_scaled); title('WM LL'); axis square;
+%subplot(2,4,2); imagesc(lh_wm_scaled); title('WM LH'); axis square;
+%subplot(2,4,3); imagesc(hl_wm_scaled); title('WM HL'); axis square;
+%subplot(2,4,4); imagesc(hh_wm_scaled); title('WM HH'); axis square;
+%subplot(2,4,5); imagesc(ll_cover_scaled); title('ORG LL'); axis square;
+%subplot(2,4,6); imagesc(lh_cover_scaled); title('ORG LH'); axis square;
+%subplot(2,4,7); imagesc(hl_cover_scaled); title('ORG HL'); axis square;
+%subplot(2,4,8); imagesc(hh_cover_scaled); title('ORG HH'); axis square;
+
+% merge LL bands of two images
+k = double(k); q = double(q);
+% resize wm to fit cover's dimensions
+[x,y,z] = size(ll_cover);
+ll_cover_size = [x, y];
+[x,y,z] = size(ll_wm);
+ll_wm_size = [x, y];
+ll_wm_resized = imresize(ll_wm, ll_cover_size);
+
+% apply k-q
+ll_result = (k * ll_cover) + (q * ll_wm_resized);
+
+% convert to 1D to reconstruct c_result
+ll_result_1d = ll_result(:)';
+
+% replace c_cover's first elements with ll_result
+ll_result_1d_size = size(ll_result_1d);
+c_result = c_cover; % initiate
+for id=1:ll_result_1d_size(2) % replace LL values
+    c_result(1,id) = ll_result_1d(1,id);
+end
+
+%[ll_result, lh_cover, hl_cover, hh_cover];
+s_result = s_cover;
+result = waverec2(c_result, s_result, fam);
+wimage = uint8(result);
+
+% calculate mse/psnr
+%D = abs(wimage - cover) .^2;
+mse  = immse(cover, wimage);
+psnr = psnr(cover, wimage);
+
+% show image and mse/psnr
+figure
+subplot(1,2,1);
+imshow(cover); title("Original"); axis square;
+subplot(1,2,2);
+imshow(wimage); colormap(gray); axis square;
+msg = strcat("MSE: ", num2str(mse), " | PSNR: ", num2str(psnr));
+title({"Watermarked";msg});
+
+% save image locally
+imwrite(wimage, 'watermarked.png');
+
+%=======================================================%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%     DEWATERMARKING    %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% read res
+% dwt 3-L res
+% ext = [LL_3(res) - k*LL_3(cover)] / q
+% other bands = 0
+% compare ext and wm
+% mse/psnr
+% check results after gaussian/white noise
+
+% clear variables
+clearvars -except k q lvl fam ll_cover wm rng;
+
+% read watermarked image
+wimage = imread('watermarked.png');
+
+% dwt 2D watermarked image
+[c_wi, s_wi] = wavedec2(wimage, lvl, fam);
+
+% extract watermarked image's LL_3 coeffs
+ll_wi = appcoef2(c_wi, s_wi, fam, lvl);
+
+% apply extraction formula
+ll_ext = (ll_wi - (k * ll_cover)) / q;
+% convert to 1D to use in c_ext
+ll_ext_1d = ll_ext(:)';
+ll_ext_1d_size = size(ll_ext_1d);
+
+c_ext = c_wi;
+s_ext = s_wi;
+
+for id = 1:ll_ext_1d_size(2) % insert result's LL
+    c_ext(id) = ll_ext_1d(id);
+end
+
+c_ext_size = size(c_ext);
+for id = ll_ext_1d_size(2)+1:c_ext_size(2) % insert result's LL
+    c_ext(id) = 0;
+end
+
+% idwt on the result array
+ext = waverec2(c_ext, s_ext, fam);
+ext = uint8(ext);
+
+% since watermark was resized in previous step,
+% we need to resize it again to calculate mse
+[x,y,z] = size(ext);
+ext_size = [x, y];
+wm_resized = imresize(wm, ext_size);
+
+% compare extracted and original wm
+% calculate mse/psnr
+mse  = immse(wm_resized, ext);
+psnr = psnr(wm_resized, ext);
+
+% show results
+figure
+subplot(1,2,1);
+imshow(wm_resized); title("Original Watermark"); axis square;
+subplot(1,2,2);
+imshow(ext); colormap(gray); axis square;
+msg = strcat("MSE: ", num2str(mse), " | PSNR: ", num2str(psnr));
+title({"Extracted Watermark";msg});
+
+% save image locally
+imwrite(ext, 'extracted_watermark.png');
+
